a9b082ff6132dbf2302886bf15bea872f482434e
Xiangyun Huang <xiangyunfaith@outlook.com>
2020-05-23 03:15:08 -0400

    修复错误

--- _common.R
+++ _common.R
@@ -1,2 +1,9 @@
+knitr::opts_chunk$set(width = 69, dpi = 300, message = FALSE, fig.align='center')
 
-knitr::opts_chunk$set(width = 69, message = FALSE)
+# convert pdf to png
+to_png <- function(fig_path) {
+  png_path <- sub("\\.pdf$", ".png", fig_path)
+  magick::image_write(magick::image_read_pdf(fig_path), format = "png", path = png_path,
+                      density = 300, quality = 100)
+  return(png_path)
+}
