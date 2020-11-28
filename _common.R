ce7f1c4422705f28b028a1e28c660db1ba791228
Xiangyun Huang <xiangyunfaith@outlook.com>
2020-11-28 18:08:56 +0800

    输出控制不了

--- _common.R
+++ _common.R
@@ -18,16 +18,3 @@ embed_math_fonts <- function(fig_path) {
   }
   return(fig_path)
 }
-
-# set a new output hook to truncate text output
-knitr::knit_hooks$set(output = function(x, options) {
-  if (!is.null(n <- options$out.lines)) {
-    x <- xfun::split_lines(x)
-    if (length(x) > n) {
-      # truncate the output
-      x <- c(head(x, n), "....\n")
-    }
-    x <- paste(x, collapse = "\n")
-  }
-  hook_output(x, options)
-})
