d5434b9a31d83bacc9e7f4e711989b1585e0604f
Xiangyun Huang <xiangyunfaith@outlook.com>
2021-01-31 17:09:49 +0800

    以后可以考虑 bs4Dash

--- dashboard/004-shinydashboard/app.R
+++ dashboard/004-shinydashboard/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+# library(bs4Dash)
 library(shinydashboard)
 
 # A dashboard body with a row of infoBoxes and valueBoxes, and two rows of boxes
@@ -7,44 +8,54 @@ body <- dashboardBody(
   # infoBoxes
   fluidRow(
     infoBox(
-      "Orders", uiOutput("orderNum2"), "Subtitle", icon = icon("credit-card")
+      "订单量", uiOutput("orderNum2"), "副标题",
+      icon = icon("credit-card")
     ),
     infoBox(
-      "Approval Rating", "60%", icon = icon("line-chart"), color = "green",
+      "访购率", "60%",
+      icon = icon("line-chart"), color = "green",
       fill = TRUE
     ),
     infoBox(
-      "Progress", uiOutput("progress2"), icon = icon("users"), color = "purple"
+      "进度", uiOutput("progress2"),
+      icon = icon("users"), color = "purple"
     )
   ),
 
   # valueBoxes
   fluidRow(
     valueBox(
-      uiOutput("orderNum"), "New Orders", icon = icon("credit-card"),
+      uiOutput("orderNum"), "新订单",
+      icon = icon("credit-card"),
       href = "https://bookdown.org/"
     ),
     valueBox(
-      tagList("60", tags$sup(style="font-size: 20px", "%")),
-      "Approval Rating", icon = icon("line-chart"), color = "green"
+      tagList("60", tags$sup(style = "font-size: 20px", "%")),
+      "访购率",
+      icon = icon("line-chart"), color = "green"
     ),
     valueBox(
-      htmlOutput("progress"), "Progress", icon = icon("users"), color = "purple"
+      htmlOutput("progress"), "进度",
+      icon = icon("users"), color = "purple"
     )
   ),
 
   # Boxes
   fluidRow(
-    box(status = "primary",
-        sliderInput("orders", "Orders", min = 1, max = 2000, value = 650),
-        selectInput("progress", "Progress",
-                    choices = c("0%" = 0, "20%" = 20, "40%" = 40, "60%" = 60, "80%" = 80,
-                                "100%" = 100)
+    box(
+      status = "primary",
+      sliderInput("orders", "订单", min = 1, max = 2000, value = 650),
+      selectInput("progress", "进度",
+        choices = c(
+          "0%" = 0, "20%" = 20, "40%" = 40, "60%" = 60, "80%" = 80,
+          "100%" = 100
         )
+      )
     ),
-    box(title = "Histogram box title",
-        status = "warning", solidHeader = TRUE, collapsible = TRUE,
-        plotOutput("plot", height = 250)
+    box(
+      title = "直方图",
+      status = "warning", solidHeader = TRUE, collapsible = TRUE,
+      plotOutput("plot", height = 250)
     )
   ),
 
@@ -52,7 +63,7 @@ body <- dashboardBody(
   fluidRow(
     # Box with textOutput
     box(
-      title = "Status summary",
+      title = "状态汇总",
       background = "green",
       width = 4,
       textOutput("status")
@@ -60,7 +71,7 @@ body <- dashboardBody(
 
     # Box with HTML output, when finer control over appearance is needed
     box(
-      title = "Status summary 2",
+      title = "状态汇总 2",
       width = 4,
       background = "red",
       uiOutput("status2")
@@ -81,73 +92,84 @@ header <- dashboardHeader(
   title = "公司仪表盘",
 
   # Dropdown menu for messages
-  dropdownMenu(type = "messages", badgeStatus = "success",
-               messageItem("Support Team",
-                           "This is the content of a message.",
-                           time = "5 mins"
-               ),
-               messageItem("Support Team",
-                           "This is the content of another message.",
-                           time = "2 hours"
-               ),
-               messageItem("New User",
-                           "Can I get some help?",
-                           time = "Today"
-               )
+  dropdownMenu(
+    type = "messages", badgeStatus = "success",
+    messageItem("Support Team",
+      "This is the content of a message.",
+      time = "5 mins"
+    ),
+    messageItem("Support Team",
+      "This is the content of another message.",
+      time = "2 hours"
+    ),
+    messageItem("New User",
+      "Can I get some help?",
+      time = "Today"
+    )
   ),
 
   # Dropdown menu for notifications
-  dropdownMenu(type = "notifications", badgeStatus = "warning",
-               notificationItem(icon = icon("users"), status = "info",
-                                "5 new members joined today"
-               ),
-               notificationItem(icon = icon("warning"), status = "danger",
-                                "Resource usage near limit."
-               ),
-               notificationItem(icon = icon("shopping-cart", lib = "glyphicon"),
-                                status = "success", "25 sales made"
-               ),
-               notificationItem(icon = icon("user", lib = "glyphicon"),
-                                status = "danger", "You changed your username"
-               )
+  dropdownMenu(
+    type = "notifications", badgeStatus = "warning",
+    notificationItem(
+      icon = icon("users"), status = "info",
+      "5 new members joined today"
+    ),
+    notificationItem(
+      icon = icon("warning"), status = "danger",
+      "Resource usage near limit."
+    ),
+    notificationItem(
+      icon = icon("shopping-cart", lib = "glyphicon"),
+      status = "success", "25 sales made"
+    ),
+    notificationItem(
+      icon = icon("user", lib = "glyphicon"),
+      status = "danger", "You changed your username"
+    )
   ),
 
-  # Dropdown menu for tasks, with progress bar
-  dropdownMenu(type = "tasks", badgeStatus = "danger",
-               taskItem(value = 20, color = "aqua",
-                        "Refactor code"
-               ),
-               taskItem(value = 40, color = "green",
-                        "Design new layout"
-               ),
-               taskItem(value = 60, color = "yellow",
-                        "Another task"
-               ),
-               taskItem(value = 80, color = "red",
-                        "Write documentation"
-               )
+  # 下拉菜单 进度条 Dropdown menu for tasks, with progress bar
+  dropdownMenu(
+    type = "tasks", badgeStatus = "danger",
+    taskItem(
+      value = 20, color = "aqua",
+      "重构代码"
+    ),
+    taskItem(
+      value = 40, color = "green",
+      "新布局设计"
+    ),
+    taskItem(
+      value = 60, color = "yellow",
+      "另一个任务"
+    ),
+    taskItem(
+      value = 80, color = "red",
+      "写文档"
+    )
   )
 )
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    menuItem("面板1", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("面板2", tabName = "widgets", icon = icon("th"))
   )
 )
 
 
 server <- function(input, output) {
   output$orderNum <- renderText({
-    prettyNum(input$orders, big.mark=",")
+    prettyNum(input$orders, big.mark = ",")
   })
 
   output$orderNum2 <- renderText({
-    prettyNum(input$orders, big.mark=",")
+    prettyNum(input$orders, big.mark = ",")
   })
 
   output$progress <- renderUI({
-    tagList(input$progress, tags$sup(style="font-size: 20px", "%"))
+    tagList(input$progress, tags$sup(style = "font-size: 20px", "%"))
   })
 
   output$progress2 <- renderUI({
@@ -155,15 +177,17 @@ server <- function(input, output) {
   })
 
   output$status <- renderText({
-    paste0("There are ", input$orders,
-           " orders, and so the current progress is ", input$progress, "%.")
+    paste0(
+      "There are ", input$orders,
+      " orders, and so the current progress is ", input$progress, "%."
+    )
   })
 
   output$status2 <- renderUI({
     iconName <- switch(input$progress,
-                       "100" = "ok",
-                       "0" = "remove",
-                       "road"
+      "100" = "ok",
+      "0" = "remove",
+      "road"
     )
     p("Current status is: ", icon(iconName, lib = "glyphicon"))
   })
@@ -179,7 +203,7 @@ shinyApp(
     header = header,
     sidebar = sidebar,
     body = body,
-    title = '可视化系统'
+    title = "可视化系统"
   ),
   server = server
 )
